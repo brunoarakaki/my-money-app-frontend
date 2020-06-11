0a466567cd6c2b9598fb48ee334e31a60c443c37
Bruno Arakaki <brn.arakaki@gmail.com>
2020-06-11 11:45:55 -0300

    add menu item and menu tree

--- src/common/template/dependencies.js
+++ src/common/template/dependencies.js
@@ -1,4 +1,4 @@
-import 'modules/admin-lte/plugins/jQueryUi/jquery-ui.min'
+import 'modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
 import 'modules/admin-lte/plugins/fastclick/fastclick'
 import 'modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
 import 'modules/admin-lte/dist/js/app.min'

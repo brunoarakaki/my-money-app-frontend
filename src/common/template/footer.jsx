5be09fc1f5e7d7f492f06478f725f1512aafab62
Bruno Arakaki <brn.arakaki@gmail.com>
2020-06-12 18:53:28 -0300

    add footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2020
+      <br></br>
+      <a href='http://github.com/brunoarakaki' target='_blank'>Bruno Arakaki</a>.
+    </strong>
+  </footer>
+)
